9f44ae94a71702ca5fb3dccb77ddb67fe073cda2
hwd404 <42717424+hwd404@users.noreply.github.com>
2022-01-08 00:28:40 -0600

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -174,16 +174,6 @@ def rain():
     return model, data
 
 
-def claim():
-    attrs = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'A11', 'A12', 'A13', 'A14', 'A15', 'A16',
-    'A17', 'A18', 'A19', 'A20', 'A21', 'A22', 'A23', 'A24', 'A25', 'A26', 'A27', 'A28', 'A29', 'A30']
-    nums = attrs
-    model = Classifier(attrs=attrs, numeric=nums, label='Target', pos='Likely_DENIED')
-    data = model.load_data('data/claim/claim_data.csv')
-    print('\n% rain dataset', len(data), len(data[0]))
-    return model, data
-
-
 def heloc():
     attrs = ['ExternalRiskEstimate', 'MSinceOldestTradeOpen', 'MSinceMostRecentTradeOpen', 'AverageMInFile',
     'NumSatisfactoryTrades', 'NumTrades60Ever2DerogPubRec', 'NumTrades90Ever2DerogPubRec', 'PercentTradesNeverDelq',
