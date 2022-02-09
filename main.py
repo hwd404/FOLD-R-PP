1629e734f6637412521b0bd48f3e14e4d3d37c7e
hwd404 <42717424+hwd404@users.noreply.github.com>
2022-02-09 16:47:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def main():
     model.fit(X_train, Y_train, ratio=0.5)
     end = timer()
 
-    model.print_asp()
+    model.print_asp(simple=True)
     Y_test_hat = model.predict(X_test)
     acc, p, r, f1 = get_scores(Y_test_hat, Y_test)
     print('% acc', round(acc, 4), 'p', round(p, 4), 'r', round(r, 4), 'f1', round(f1, 4))
