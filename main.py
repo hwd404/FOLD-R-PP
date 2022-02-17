673cceac9392e7d3eb012dfa4a34ab75982c0cfb
hwd404 <42717424+hwd404@users.noreply.github.com>
2022-02-17 13:38:25 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from datetime import timedelta
 def main():
     # model, data = acute()
     # model, data = autism()
-    model, data = breastw()
-    # model, data = cars()
+    # model, data = breastw()
+    model, data = cars()
     # model, data = credit()
     # model, data = heart()
     # model, data = kidney()
@@ -22,27 +22,33 @@ def main():
     # model, data = adult()
     # model, data = credit_card()
     # model, data = rain()
-    # model, data = heloc()
+    # model, data = parkison()
 
     data_train, data_test = split_data(data, ratio=0.8, rand=True)
+    # 80% as training data, 20% as test data. shuffle data first when rand is True
 
     # model, data_train, data_test = titanic()
+    # model, data_train, data_test = avila()
+    # model, data_train, data_test = anneal()
 
-    X_train, Y_train = split_xy(data_train)
+    X_train, Y_train = split_xy(data_train)  # split data into features and label
     X_test,  Y_test = split_xy(data_test)
 
     start = timer()
     model.fit(X_train, Y_train, ratio=0.5)
+    # ratio means # of exception examples / # of default examples a rule can imply = 0.5
+    
     end = timer()
-
     model.print_asp(simple=True)
+    # print simplified rules when simple is True, default value is False
+    
     Y_test_hat = model.predict(X_test)
     acc, p, r, f1 = get_scores(Y_test_hat, Y_test)
     print('% acc', round(acc, 4), 'p', round(p, 4), 'r', round(r, 4), 'f1', round(f1, 4))
     print('% foldr++ costs: ', timedelta(seconds=end - start), '\n')
 
     # k = 1
-    # for i in range(len(X_test)):
+    # for i in range(10):
     #     print('Explanation for example number', k, ':')
     #     print(model.explain(X_test[i], all_flag=False))
     #     print('Proof Trees for example number', k, ':')
